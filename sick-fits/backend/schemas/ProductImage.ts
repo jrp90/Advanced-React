e854b33f385f3d4e1d05d8597c535e3e873192dd
James Pettifer <jpettifer@helical-levity.com>
2021-02-04 20:15:58 +0000

    Module 4

---
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -0,0 +1,29 @@
+import { list } from '@keystone-next/keystone/schema';
+import { relationship, text } from '@keystone-next/fields';
+import { cloudinaryImage } from '@keystone-next/cloudinary';
+import 'dotenv/config';
+
+export const cloudinary = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: 'sick-fits',
+};
+
+export const ProductImage = list({
+  // access:
+  // ui:
+  fields: {
+    image: cloudinaryImage({
+      cloudinary,
+      label: 'Source',
+    }),
+    altText: text(),
+    product: relationship({ ref: 'Product.photo' }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['image', 'altText', 'product'],
+    },
+  },
+});
