c87cb2e5b333e40a64f4b1d754e38a9916ea07b9
James Pettifer <james.pettifer9@gmail.com>
2021-02-07 22:52:24 +0000

    More Module 5

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import useForm from '../lib/useForm';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, resetForm, clearForm } = useForm({
+    name: 'Nice shoes',
+    price: 3423,
+    description: 'Some really nice shoes',
+  });
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="button" onClick={clearForm}>
+        Clear form
+      </button>
+
+      <button type="button" onClick={resetForm}>
+        Reset form
+      </button>
+    </form>
+  );
+}
